76940b669b9e46f11cb3ebe90c22cefbb8d44417
Aditya Joshi <adityajoshi.sfdc@gmail.com>
2023-09-24 17:45:48 -0400

    schema fixed and project file structure refactored

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,10 +1,19 @@
 const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Author" type defines the queryable fields for every Author in our data source.
-  type Author {
+
+  # This "User" type defines the queryable fields for every User in our data source.
+  type User {
     id: String!
     name: String!
+    visibility: Boolean
+  }
+
+  type Course {
+    id: String!
+    label: String!
+    author: User!
+    reviews: [Review!]
   }
 
   # This "Review" type defines the queryable fields for every Review in our data source.
@@ -12,14 +21,18 @@ const typeDefs = `#graphql
     id: String!
     rating: Int!
     content: String
-    author: Author
+    reviewer: User!
+    course: Course!
   }
+
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     reviews: [Review]
+    courses: [Course]
+    authors: [Author]
   }
 `;
 
