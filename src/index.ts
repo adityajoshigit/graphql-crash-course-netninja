096acb206b62b573c547ee0b435ce8a013d11460
Aditya Joshi <adityajoshi.sfdc@gmail.com>
2023-09-24 18:42:31 -0400

    completed basic skeleton of data definition, data plugging, resolvers etc

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,31 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import typeDefs from './schema';
-import { reviewsData } from './data';
+import {reviews, courses, users} from './data';
+
+const PORT = 4000;
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
 
+const resolvers = {
+  Query: {
+    reviews: () => reviews,
+    courses: () => courses,
+    users: () => users,
+  }
+};
 
 const server = new ApolloServer({
-  typeDefs
+  typeDefs,
+  resolvers
 });
 
 const {url} = await startStandaloneServer(server, {
   listen: {
-    port: 4000
+    port: PORT
   }
 });
 
-console.log('Server started at PORT 4000');
+console.log(`... 🚀 \nServer started serving at PORT ${url+':'+PORT}\n...\n...`);
